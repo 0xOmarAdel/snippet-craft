0d127b727b7d70f2d9fc1f5ebe31617c710b18d9
Heisenberg <omaradelawad20013@gmail.com>
2024-01-11 14:05:04 +0200

    moved the createSnippet function to the actions file

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -3,6 +3,37 @@
 import { db } from "@/db";
 import { redirect } from "next/navigation";
 
+export const createSnippet = async (
+  formState: { message: string },
+  fromData: FormData
+) => {
+  const title = fromData.get("title");
+  const code = fromData.get("code");
+
+  if (typeof title !== "string" || title.length < 3) {
+    return { message: "Title must be longer" };
+  }
+
+  if (typeof code !== "string" || code.length < 3) {
+    return { message: "Code must be longer" };
+  }
+
+  try {
+    const snippet = await db.snippet.create({
+      data: {
+        title,
+        code,
+      },
+    });
+
+    console.log(snippet);
+  } catch (error) {
+    return { message: "Error creating snippet" };
+  }
+
+  redirect("/");
+};
+
 export const editSnippet = async (id: number, code: string) => {
   await db.snippet.update({
     where: { id },
