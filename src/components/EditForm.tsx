a780f7437e97265ef22a81a8ae874e58b4f36ed7
Heisenberg <omaradelawad20013@gmail.com>
2024-01-11 10:49:57 +0200

    added a submit form to edit the snippet

--- src/components/EditForm.tsx
+++ src/components/EditForm.tsx
@@ -3,6 +3,7 @@
 import type { Snippet } from "@prisma/client";
 import Editor from "@monaco-editor/react";
 import { useState } from "react";
+import * as actions from "@/actions";
 
 type Props = {
   snippet: Snippet;
@@ -10,6 +11,12 @@ type Props = {
 
 const EditForm = ({ snippet }: Props) => {
   const [code, setCode] = useState<string | undefined>(snippet.code);
+
+  /* const submitHandler = async () => {
+    await actions.editSnippet(snippet.id, code);
+  }; */
+
+  const submitHandler = actions.editSnippet.bind(null, snippet.id, code!);
 
   return (
     <div>
@@ -21,6 +28,12 @@ const EditForm = ({ snippet }: Props) => {
         onChange={(value) => setCode(value)}
         options={{ minimap: { enabled: false } }}
       />
+      {/* <button className="p-2 border rounded" onClick={submitHandler}>Save</button> */}
+      <form action={submitHandler}>
+        <button className="p-2 border rounded" type="submit">
+          Save
+        </button>
+      </form>
     </div>
   );
 };
