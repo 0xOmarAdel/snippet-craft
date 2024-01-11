3fb5fc6e25e67cb8cc5b605cb1af8869d3892c58
Heisenberg <omaradelawad20013@gmail.com>
2024-01-11 10:05:04 +0200

    added the code editor for the snippet

--- src/components/EditForm.tsx
+++ src/components/EditForm.tsx
@@ -1,13 +1,28 @@
 "use client";
 
 import type { Snippet } from "@prisma/client";
+import Editor from "@monaco-editor/react";
+import { useState } from "react";
 
 type Props = {
   snippet: Snippet;
 };
 
 const EditForm = ({ snippet }: Props) => {
-  return <div>{snippet.code}</div>;
+  const [code, setCode] = useState<string | undefined>(snippet.code);
+
+  return (
+    <div>
+      <Editor
+        height="40vh"
+        theme="vs-dark"
+        language="javascript"
+        defaultValue={code}
+        onChange={(value) => setCode(value)}
+        options={{ minimap: { enabled: false } }}
+      />
+    </div>
+  );
 };
 
 export default EditForm;
